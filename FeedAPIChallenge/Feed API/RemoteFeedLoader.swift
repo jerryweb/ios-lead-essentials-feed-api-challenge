81fd0b8564f1405783634e1c2500a25aebc90fa5
Jerry Webb <ljwebbdev8800@gmail.com>
2021-08-19 22:30:31 -0700

    Addressed Danil's code review suggestions

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -13,8 +13,19 @@ public final class RemoteFeedLoader: FeedLoader {
 		case invalidData
 	}
 
-	public struct Root: Decodable {
+	private struct Root: Decodable {
 		let items: [Item]
+	}
+
+	private struct Item: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+
+		var image: FeedImage {
+			return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+		}
 	}
 
 	public init(url: URL, client: HTTPClient) {
@@ -24,31 +35,20 @@ public final class RemoteFeedLoader: FeedLoader {
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { [weak self] result in
-			
+
 			guard self != nil else { return }
-			
+
 			switch result {
-				case let .success((data, response)):
-					if response.statusCode == 200, let images = try? JSONDecoder().decode(Root.self, from: data) {
-						completion(.success(images.items.map({ $0.item })))
-					} else {
-						completion(.failure(Error.invalidData))
-					}
-				case .failure:
-					completion(.failure(Error.connectivity))
+			case let .success((data, response)):
+				if response.statusCode == 200, let images = try? JSONDecoder().decode(Root.self, from: data) {
+					completion(.success(images.items.map({ $0.image })))
+				} else {
+					completion(.failure(Error.invalidData))
+				}
+			case .failure:
+				completion(.failure(Error.connectivity))
 			}
 			return
 		}
-	}
-}
-
-internal struct Item: Decodable {
-	let image_id: UUID
-	let image_desc: String?
-	let image_loc: String?
-	let image_url: URL
-
-	var item: FeedImage {
-		return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
 	}
 }
